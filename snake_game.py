bc188f519dd188d1255a5d566b6661f7ecb24d8d
Marian <ianlegaspi.marian@gmail.com>
2024-10-01 18:33:08 +0800

    added the game the initial game window

--- snake_game.py
+++ snake_game.py
@@ -1,1 +1,18 @@
-print("finally configured, gonna start writing soon")
+import tkinter 
+import random
+
+ROWS = 25
+COLS = 25
+TILE_SIZE = 25
+
+WINDOW_WIDTH = TILE_SIZE * COLS
+WINDOW_HEIGHT =  TILE_SIZE * ROWS
+
+#game window 
+game_window = tkinter.Tk()
+game_window.title("Snake")
+game_window.resizable(False, False)
+
+game_window.mainloop()
+
+
