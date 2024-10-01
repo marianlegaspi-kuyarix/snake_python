bf4910caec41cb314253173144bf0bbfa06d771e
Marian <ianlegaspi.marian@gmail.com>
2024-10-01 19:47:28 +0800

    centered the game window

--- snake_game.py
+++ snake_game.py
@@ -15,7 +15,19 @@ game_window.resizable(False, False)
 
 canvas = tkinter.Canvas(game_window, bg = "magenta", width = WINDOW_WIDTH, height = WINDOW_HEIGHT, borderwidth = 0, highlightthickness = 0)
 canvas.pack()
-game_window.update
+game_window.update()
+
+#center the game window
+window_width = game_window.winfo_width()
+window_height = game_window.winfo_height()
+screen_width = game_window.winfo_screenwidth()
+screen_height = game_window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+
+#format "(w)x(h)+h+x+y"
+game_window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 
 game_window.mainloop()
 
