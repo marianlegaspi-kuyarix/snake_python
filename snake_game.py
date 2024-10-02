680f5769259e1c6c5f507e3e75876b810ad3a674
Marian <ianlegaspi.marian@gmail.com>
2024-10-02 18:36:45 +0800

    Update on the snake, when it eats the food it grows by one tile at a time

--- snake_game.py
+++ snake_game.py
@@ -69,6 +69,16 @@ def move():
         snake_food.x = random.randint(0, COLS-1) * TILE_SIZE
         snake_food.y = random.randint(0, ROWS-1) * TILE_SIZE
 
+    #update the snake body
+    for i in range(len(snake_body)-1, -1):
+        tile = snake_body[i]
+        if (i == 0):
+            tile.x = snake.x
+            tile.y = snake.y
+        else:
+            prev_tile = snake_body[i-1]
+            tile.x = prev_tile.x
+            tile.y = prev_tile.y
 
     snake.x += snake_vel_x * TILE_SIZE
     snake.y += snake_vel_y * TILE_SIZE
