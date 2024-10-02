e5bfb2fa33ae61ee78250d51e985c9f1e7fb7b75
Marian <ianlegaspi.marian@gmail.com>
2024-10-02 17:06:17 +0800

    Added the snake's food

--- snake_game.py
+++ snake_game.py
@@ -36,12 +36,16 @@ game_window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 
 #initialize the game
 snake = Tile(5* TILE_SIZE, 5*TILE_SIZE) #single tile for the snake's head
+snake_food =Tile(10* TILE_SIZE, 10*TILE_SIZE) 
 
 def draw():
     global snake
 
     #draw the snake
     canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill = "cyan")
+
+    #draw the snake's food
+    canvas.create_rectangle(snake_food.x, snake_food.y, snake_food.x + TILE_SIZE, snake_food.y + TILE_SIZE, fill = "yellow")
 
     game_window.after(100, draw) #100ms = 1/10 second, 10 frames/second
 
